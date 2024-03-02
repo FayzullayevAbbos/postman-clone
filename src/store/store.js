d68070def46bc279aae7ffd52bff22141561ba46
FayzullayevAbbos <abbosfayzullayev202@gmail.com>
2024-03-02 11:51:08 +0500

    store

--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,5 @@ import { configureStore } from "@reduxjs/toolkit";
 import postmanSlice from "./postmanSlice";
 
 export const store = configureStore({
-  reducer: postmanSlice,
+  reducer: postmanSlice,//store
 });
